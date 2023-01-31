04e6b25143d194da26321407741f30ca116fbefa
DonzerHD <thomas.lemay59@outlook.fr>
2023-01-31 09:41:24 +0100

    remis zero

--- src/blog/url.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import index,article
-
-
-urlpatterns = [
-    path('',index, name="blog-index"),
-    path('article<int:numero_article>/',article, name="article"),
-]
